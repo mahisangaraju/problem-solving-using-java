1eca77fb969dfc9da30c849529e046da180ef5cd
mahisangaraju <mahisangaraju1@gmail.com>
2026-02-06 06:41:40 +0300

    Add files via upload

---
+++ NthFibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class NthFibonacci {
+    public static long fib(int n) {
+        if (n <= 1) return n;
+
+        long a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            long c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fib(n));
+    }
+}
